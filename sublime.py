99109e1041df4b0b2dd20991fb619990ff33b10a
Hari31416 <harikeshkumar0926@gmail.com>
2021-05-15 13:17:58 +0530

    modifications

--- sublime.py
+++ sublime.py
@@ -1,10 +1,18 @@
-def count_primes(num):
-    the_tuple = []
-    for n in range(2, num):
-        for m in range(2, num):
-            divisors = []
-            if n%m == 0:
-                divisors.append(m)
-count_primes(100)
-count = [(n,m) for n in range(2,101) m in range(2, 102) if n%m ==0]
-print(count)
+import math
+def prime(num):
+    multiples = []
+    final_num = math.floor(num**0.5)
+    for i in range(2, final_num+1):
+      if num%i ==0:
+        multiples.append(i)
+    return len(multiples) == 1
+
+def count_primes(num_range):
+    primes = []
+    for m in list(range(2, nu+1, 2)):
+        if prime(m):
+            primes.append(m)
+    return len(primes)
+num_primes = count_primes(140000)
+print(num_primes)
+
