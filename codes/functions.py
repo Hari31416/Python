cf82df657b5c7ffd158124b3108354f7ec031285
Hari31416 <harikeshkumar0926@gmail.com>
2021-05-23 13:07:27 +0530

    modifications

---
+++ codes/functions.py
@@ -0,0 +1,81 @@
+from math import sqrt
+import time
+
+def isprime(num):
+    """
+    Returns true if num is prime
+    """
+    if num == 0 or num == 1 or not num % 2:
+        if num == 2:
+            return True
+        return False
+    for i in range(3, int(sqrt(num)) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+
+def return_prime_till_n(n):
+    """
+    Returns a list of prime numbers till n
+    """
+    primes =[2]
+    for i in range(3, n, 2):
+        if isprime(i):
+            primes.append(i)
+    return primes
+
+t0 = time.time()
+print(len(return_prime_till_n(100000)))
+t1 = time.time()
+print(t1-t0)
+
+def prime_till_n(n):
+    """
+    Another method to return prime till n
+    Needs modification
+    """
+    i = 2
+    primes = list(range(2,n))
+    while True:
+        num = primes.index(i)
+        for m in range(2, int(n/i)+1):
+            if i*m in primes:
+                primes.remove(i*m)
+            else:
+                pass
+        if i != primes[-1]:
+            i = primes[num +1]
+            
+        else:
+            break
+    return primes
+
+
+def return_m_prime(m):
+    """
+    Returns mth prime
+    """
+    primes =[2]
+    i = 1
+    while len(primes)<m:
+        i = i+2
+        if isprime(i):
+            primes.append(i)
+    return primes[m-1]
+
+#All above three functions use isprime() method
+
+def factors(n):
+    """
+    Returns factors of n
+    """
+    factors =[]
+    for i in range(1, int(sqrt(n)+1)):
+        if n%i == 0:
+            factors.append(i)
+            if i*i !=n:
+                factors.append(n/i)
+    return factors
+
+
--- sublime.py
+++ sublime.py
@@ -1,23 +1,12 @@
+def factors(num):
+    factors = []
+    for n in range(1, num+1):
+        if num%n==0:
+            factors.append(n)
+    return len(factors)
+
 import time
 t0 = time.time()
-i = 3
-flage = True
-while flage:
-    tringular = []
-    numbers = list(range(1,i))
-    for n in range(1,i):
-        tringular.append(sum(numbers[0:n]))
-    n = tringular[i-2]
-    divisors_n = []
-    for m in range(1,n+1):
-        if n%m == 0:
-            m = m/n
-            divisors_n.append(m)
-    if len(divisors_n)==50:
-        print(n)
-        flage = False
-    else:
-        i+=1
-
+print(factors(27494820))
 t1 = time.time()
 print(t1-t0)
