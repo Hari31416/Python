47de52e0d92a7b5780ac4f28d1aca2721eb03078
Hari31416 <harikeshkumar0926@gmail.com>
2021-07-05 09:53:52 +0530

    Goodreads

--- codes/Web Scraping/file1.py
+++
@@ -1,8 +0,0 @@
-import requests
-import bs4
-
-code = requests.get("https://coppermind.net/wiki/Cytonic_(book)")
-soup = bs4.BeautifulSoup(code.text, "lxml")
-images = soup.select(".image")
-image = images[0]
-print(image[src])
